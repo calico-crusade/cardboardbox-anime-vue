2a671a96cc10a3c3eff1278834a7d10371895a79
Cardboard <31460039+calico-crusade@users.noreply.github.com>
2023-05-07 04:14:50 -0400

    adding SEO for reverse search

--- src/pages/reverse/[url].vue
+++ src/pages/reverse/[url].vue
@@ -16,12 +16,17 @@
 <script setup lang="ts">
 const route = useRoute();
 const { reverseFile, reverseUrl } = useMangaApi();
-const { toPromise } = useApiHelper();
+const { toPromise, proxy } = useApiHelper();
 
 const url = computed(() => decodeURIComponent(route.params.url?.toString() ?? ''));
 const search = ref(url.value);
-const { pending, data: results } = reverseUrl(url.value);
+const { pending, data: results } = await reverseUrl(url.value);
 const combined = computed(() => results.value ? [...results.value.match, ...results.value.vision, ...results.value.textual] : []);
+const first = computed(() => combined.value[0]);
+
+const title = computed(() => first.value?.manga?.title ?? 'Reverse Image Search');
+const description = computed(() => first.value?.manga.description ?? 'Reverse Image search Manga pages to find the manga source.');
+const cover = computed(() => first.value?.manga.cover ? proxy(first.value.manga.cover, 'manga-cover') : 'https://manga.index-0.com/logo.png');
 
 const searchFile = async (file: File) => {
     results.value = null;
@@ -30,8 +35,6 @@ const searchFile = async (file: File) => {
     pending.value = false;
 }
 
-const title = 'Reverse Image Search', description = 'Reverse Image search Manga pages to find the manga source.', image = 'https://manga.index-0.com/logo.png';
-
 useHead({ title });
 
 useServerSeoMeta({
@@ -39,7 +42,7 @@ useServerSeoMeta({
     ogTitle: title,
     description,
     ogDescription: description,
-    ogImage: image,
+    ogImage: cover,
     twitterCard: 'summary_large_image'
 });
 </script>
